49dddc2838dc3cf4d6bd3889dfa468278c07c682
Alexandre Halm <alexandrehalm@gmail.com>
2015-12-13 15:13:31 +0400

    shaved off 4 bytes from file 1 and added new shorter version

--- golf-convexhull.R
+++ golf-convexhull.R
@@ -1,8 +1,8 @@
 d=read.csv(file("stdin"),sep=";",header=F)    
 p=function(a,b) a[1]*b[1]+a[2]*b[2]
-t=function(x,a,b,c) {
+t=function(a,b,c) {
 A=d[a,]
-U=d[x,]-A
+U=d[1,]-A
 B=d[b,]-A
 C=d[c,]-A
 f=p(C,C)
@@ -14,4 +14,4 @@ k=f*i-g*g
 u=i*h-g*j
 v=f*j-g*h
 min(u*k,v*k,k-u-v)>0}
-any(apply(combn(2:dim(d)[1],3),2,function(v) t(1,v[1],v[2],v[3])))
+any(apply(combn(2:dim(d)[1],3),2,function(v) t(v[1],v[2],v[3])))
