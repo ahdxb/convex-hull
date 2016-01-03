ec57542b77c52a0ccbc4b27634b14cc34daa7ab1
Alexandre Halm <alexandrehalm@gmail.com>
2016-01-03 13:02:30 +0400

    Final golf
    removed extra spaces and useless argument names

--- golf-convexhull-2.R
+++ golf-convexhull-2.R
@@ -1,4 +1,4 @@
-d=read.csv(file("stdin"),sep=";",header=F)    
-q=function(i,j,k) abs(det(as.matrix(cbind(d[c(i,j,k),],1))))
-t=function(i,j,k) q(i,j,k)-q(1,i,j)-q(1,i,k)-q(1,j,k)==0
-any(apply(combn(2:dim(d)[1],3),2,function(v) t(v[1],v[2],v[3])))
+d=read.csv(file("stdin"),F,";")
+q=function(i,j,k)abs(det(as.matrix(cbind(d[c(i,j,k),],1))))
+t=function(i,j,k)q(i,j,k)==q(1,i,j)+q(1,i,k)+q(1,j,k)
+any(apply(combn(2:nrow(d),3),2,function(v)t(v[1],v[2],v[3])))
--- golf-convexhull.R
+++ golf-convexhull.R
@@ -1,7 +1,6 @@
-d=read.csv(file("stdin"),sep=";",header=F)    
-p=function(a,b) a[1]*b[1]+a[2]*b[2]
-t=function(a,b,c) {
-A=d[a,]
+d=read.csv(file("stdin"),F,";")
+p=function(a,b)a[1]*b[1]+a[2]*b[2]
+t=function(a,b,c){A=d[a,];
 U=d[1,]-A
 B=d[b,]-A
 C=d[c,]-A
@@ -14,4 +13,4 @@ k=f*i-g*g
 u=i*h-g*j
 v=f*j-g*h
 min(u*k,v*k,k-u-v)>0}
-any(apply(combn(2:dim(d)[1],3),2,function(v) t(v[1],v[2],v[3])))
+any(apply(combn(2:nrow(d),3),2,function(v)t(v[1],v[2],v[3])))
